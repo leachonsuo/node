0fe7589dcbdef71429cfaabb6f7aaf32d7d07428
lee chan seo <42969429+leachonsuo@users.noreply.github.com>
2022-08-05 20:18:52 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var http = require('http')
+var fs = require('fs')
+var app = http.createServer(function(request , response){
+    var template = 
+    `
+    <html>
+    <head>
+        <title>이찬서</title>
+    </head>
+    <body>
+        <h1>노드js</h1>
+        <p> 첫 번째 강의</p>
+    </body>
+    </html>
+    `
+    if(request.url == "/"){
+        url = '/index.html'
+    }
+    response.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+    response.end(fs.readFileSync(__dirname+url))
+})
+
+app.listen(3000)
